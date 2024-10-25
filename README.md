6108b75ecc3eaab86a8b3467db63a2a36bc36739
Febryan Putra Kartika <febryanputra12@gmail.com>
2024-10-25 23:15:58 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# ConVIRT
-Contrastive Learning of Medical Visual Representations from Paired Images and Text
+# ConVIRT - Contrastive Learning Representations of Images and Text pairs
+
+### Contrastive VIsual Representation Learning from Text
+
+Deep neural networks learn from a large amount of data to obtain the correct parameters to perform a speciﬁc task. However, in practice, we often encounter a problem: **insuﬃcient amount of labeled data**. However, if your data contains pairs of images and text, you can solve the problem with contrastive learning.
+
+Contrastive learning is a kind of self-supervised learning method. It does not require specialized labels, but rather a method to learn the correct parameters from the unlabeled data itself. It aims to learn an encoder that makes the encoding results of similar classes of data similar and makes the encoding results of diﬀerent classes of data as diﬀerent as possible. Typical contrast learning is done based on comparisons between two images. However, if we have paired image and text data, contrast learning can also be applied between images and text.
+
+The repository is a PyTorch implementation of the architecture descibed in the ConVIRT paper: [_Contrastive Learning of Medical Visual Representations from Paired Images and Text_](https://arxiv.org/abs/2010.00747). The authors of paper are Yuhao Zhang, Hang Jiang, Yasuhide Miura, Christopher D. Manning, Curtis P. Langlotz.
+
+This repository was originally modified from https://github.com/edreisMD/ConVIRT-pytorch.
+
+References:
+
+- Yuhao Zhang et al. Contrastive Learning of Medical Visual Representations from Paired Images and Text. https://arxiv.org/pdf/2010.00747.pdf
+- Ting Chen et al. A Simple Framework for Contrastive Learning of Visual Representations. https://arxiv.org/abs/2002.05709
+- https://github.com/sthalles/SimCLR
+- https://github.com/google-research/simclr
+- https://github.com/google-research/bert
+- https://github.com/hanxiao/bert-as-service#q-what-are-the-available-pooling-strategies
---
+++ train.py
@@ -0,0 +1,207 @@
+import logging
+import os
+import shutil
+
+import numpy as np
+import torch
+from torch.cuda.amp import GradScaler, autocast
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+from transformers import AutoTokenizer
+
+from loss.nt_xent import NTXentLoss
+
+# from models.resnet_clr import ResNetSimCLR
+from models.model import ModelCLR
+
+logging.getLogger("transformers.tokenization_utils_base").setLevel(logging.ERROR)
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+torch.manual_seed(42)
+
+
+def _save_config_file(model_checkpoints_folder):
+    if not os.path.exists(model_checkpoints_folder):
+        os.makedirs(model_checkpoints_folder)
+        shutil.copy(
+            "./config.yaml", os.path.join(model_checkpoints_folder, "config.yaml")
+        )
+
+
+class SimCLR(object):
+    def __init__(self, dataset, config):
+        self.config = config
+        self.device = self._get_device()
+        self.writer = SummaryWriter()
+        self.dataset = dataset
+        self.nt_xent_criterion = NTXentLoss(
+            self.device, config["batch_size"], **config["loss"]
+        )
+        self.truncation = config["truncation"]
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            config["model"]["bert_base_model"]
+        )  # , do_lower_case=config['model_bert']['do_lower_case'])
+
+    def _get_device(self):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        print("Running on:", device)
+        return device
+
+    def train(self):
+        # Dataloaders
+        train_loader, valid_loader = self.dataset.get_data_loaders()
+
+        # Model Resnet Initialize
+        model = ModelCLR(**self.config["model"]).to(self.device)
+        model = self._load_pre_trained_weights(model)
+
+        optimizer = torch.optim.Adam(
+            model.parameters(),
+            eval(self.config["learning_rate"]),
+            weight_decay=eval(self.config["weight_decay"]),
+        )
+
+        # # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,
+        #                                                         T_max=len(train_loader), b
+        #                                                         eta_min=0,
+        #                                                         last_epoch=-1)
+
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, "min", factor=0.5, patience=5
+        )
+
+        scaler = GradScaler()
+
+        # Checkpoint folder
+        model_checkpoints_folder = os.path.join(self.writer.log_dir, "checkpoints")
+
+        # save config file
+        _save_config_file(model_checkpoints_folder)
+
+        n_iter = 0
+        valid_n_iter = 0
+        best_valid_loss = np.inf
+
+        print("Training...")
+        model.train()
+        for epoch_counter in range(1, (self.config["epochs"] + 1)):
+            epoch_loss = 0.0
+            train_counter = 0
+            # print(f'Epoch {epoch_counter}')
+            for xis, xls in tqdm(train_loader):
+                optimizer.zero_grad()
+                # optimizer_bert.zero_grad()
+
+                xls = self.tokenizer(
+                    list(xls),
+                    return_tensors="pt",
+                    padding=True,
+                    truncation=self.truncation,
+                )
+
+                xis = xis.to(self.device)
+                xls = xls.to(self.device)
+
+                # Only use autocast for the forward pass
+                with autocast():
+                    # get the representations and the projections (Mixed Precision Training)
+                    zis, zls = model(xis, xls)  # [N,C]
+
+                    # get the representations and the projections
+                    # zls = model_bert(xls)  # [N,C]
+                    # zls = xls
+                    # normalize projection feature vectors
+
+                    loss = self.nt_xent_criterion(zis, zls)
+
+                    # loss = self._step(model_res, model_bert, xis, xls, n_iter)
+
+                # Scales the loss to create scaled gradients
+                scaler.scale(loss).backward()
+
+                # Unscales the gradients
+                scaler.step(optimizer)
+
+                # optimizer_bert.step()
+                # optimizer.step()
+
+                # Update the scale for next iteration
+                scaler.update()
+                # n_iter += 1
+                epoch_loss += loss.item()
+                train_counter += 1
+
+            # Calculate avg loss for the epoch
+            epoch_loss /= train_counter
+
+            # Log training loss
+            self.writer.add_scalar("train_loss", epoch_loss, global_step=epoch_counter)
+
+            # Log lr
+            current_lr = optimizer.param_groups[0]["lr"]
+            self.writer.add_scalar(
+                "lr_decay_on_plateau", current_lr, global_step=epoch_counter
+            )
+
+            print(f"Epoch {epoch_counter} ------ Train Loss: {epoch_loss}")
+
+            # validate the model if requested
+            if epoch_counter % self.config["eval_every_n_epochs"] == 0:
+                valid_loss = self._validate(model, valid_loader, n_iter)
+                scheduler.step(valid_loss)
+                if valid_loss < best_valid_loss:
+                    # save the model weights
+                    best_valid_loss = valid_loss
+                    torch.save(
+                        model.state_dict(),
+                        os.path.join(model_checkpoints_folder, "model.pth"),
+                    )
+                self.writer.add_scalar(
+                    "validation_loss", valid_loss, global_step=epoch_counter
+                )
+                valid_n_iter += 1
+                print(f"Validation {epoch_counter} - Valid Loss: {valid_loss}")
+
+    def _load_pre_trained_weights(self, model):
+        try:
+            checkpoints_folder = os.path.join(
+                "./runs", self.config["fine_tune_from"], "checkpoints"
+            )
+            state_dict = torch.load(os.path.join(checkpoints_folder, "model.pth"))
+            model.load_state_dict(state_dict)
+            print("Loaded pre-trained model with success.")
+        except FileNotFoundError:
+            print("Pre-trained weights not found. Training from scratch.")
+
+        return model
+
+    def _validate(self, model, valid_loader, n_iter):
+        # validation steps
+        with torch.no_grad():
+            model.eval()
+            # model_bert.eval()
+            valid_loss = 0.0
+            counter = 0
+            # print(f'Validation step')
+            for xis, xls in tqdm(valid_loader):
+                xls = self.tokenizer(
+                    list(xls),
+                    return_tensors="pt",
+                    padding=True,
+                    truncation=self.truncation,
+                )
+
+                xis = xis.to(self.device)
+                xls = xls.to(self.device)
+
+                # get the representations and the projections
+                zis, zls = model(xis, xls)  # [N,C]
+
+                loss = self.nt_xent_criterion(zis, zls)
+
+                valid_loss += loss.item()
+                counter += 1
+            valid_loss /= counter
+        model.train()
+        # model_bert.train()
+        return valid_loss
